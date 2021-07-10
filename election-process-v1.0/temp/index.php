2cbe1f4ff2bbf1544dbce5b191f176d742ef6926
MekadAlIibrahem <79482795+MekadAlibrahem@users.noreply.github.com>
2021-07-10 23:12:13 +0300

    temp file
    1

---
+++ election-process-v1.0/temp/index.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="ar">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cheatsheet · Bootstrap v5.0</title>
+
+    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cheatsheet/"> -->
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../lib/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <style>
+      .nav-pills .nav-link:hover{
+        background-color:#0dcaff;
+        color :#fff;
+      }
+    </style>
+      
+      <!-- Custom styles for this template -->
+      <link href="../lib/Css/headers.css" rel="stylesheet">
+
+    
+    <!-- Custom styles for this template -->
+    <link href="cheatsheet.css" rel="stylesheet">
+    <style>
+      .container{
+        display: contents;
+      }
+      
+      
+    </style>
+  </head>
+  <body class="">
+  <div class="container">
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bd-header">
+    
+  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      
+      <span class="fs-4"> موقع أتمتة انتخابات </span>
+  </a>
+
+  <ul class="nav nav-pills">
+
+      <li class="nav-item"><a href="#" class="nav-link"> حول الموقع </a></li>
+      <li class="nav-item"><a href="normaluser/result.php" class="nav-link">نتائج الانتخابات</a></li>
+      <li class="nav-item"><a href="sign-in.php" class="nav-link">تسجيل الدخول</a></li>
+      <li class="nav-item"><a href="#" class="nav-link active">الصفحة الرئيسية</a></li>
+      
+    </ul>
+    
+  </header>
+</div>
+<div id="about" style=" display: inline-flex !important; margin-top: 80px !important;
+        margin-left: 0px !important;
+        width: max-content !important;">
+<aside class="bd-aside sticky-xl-top text-muted align-self-start mb-3 mb-xl-5 px-2">
+  
+  <nav class="small" id="toc">
+    <ul class="list-unstyled">
+      <li class="my-2">
+        <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#contents-collapse" aria-controls="contents-collapse">Contents</button>
+        <ul class="list-unstyled ps-3 collapse" id="contents-collapse">
+          <li><a class="d-inline-flex align-items-center rounded" href="#typography">Typography</a></li>
+          
+        </ul>
+      </li>
+      <li class="my-2">
+        <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#forms-collapse" aria-controls="forms-collapse">Forms</button>
+        <ul class="list-unstyled ps-3 collapse" id="forms-collapse">
+          <li><a class="d-inline-flex align-items-center rounded" href="#overview">Overview</a></li>
+          
+        </ul>
+      </li>
+      <li class="my-2">
+        <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#components-collapse" aria-controls="components-collapse">Components</button>
+        <ul class="list-unstyled ps-3 collapse" id="components-collapse">
+          <li><a class="d-inline-flex align-items-center rounded" href="#accordion">Accordion</a></li>
+          
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</aside>
+<div class="bd-cheatsheet container-fluid bg-body">
+  mmmmmmmm
+  mmmmmmmmm
+  mmmmmmmmmm
+  mmmmmmmmmm
+  mmmmmmmmmm
+  mmmmmmmmmm
+  
+</div>
+
+    </div>
+
+    <script src="../lib/assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      <script src="cheatsheet.js"></script>
+  </body>
+</html>
