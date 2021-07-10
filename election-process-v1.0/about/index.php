eeaf9e8f5aa6c44a4bab64d4bfadac29a3c1c2fd
MekadAlIibrahem <79482795+MekadAlibrahem@users.noreply.github.com>
2021-07-10 23:30:51 +0300

    about
    1

---
+++ election-process-v1.0/about/index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="ar">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>  حول الموقع </title>
+
+    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cheatsheet/"> -->
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../lib/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <style>
+      .nav-pills .nav-link:hover{
+        background-color:#0dcaff;
+        color :#fff;
+
+      }
+      .information{
+        text-align: right !important;
+        margin-left: 12%;
+        margin-right: 12%;
+      }
+      .information::before{
+        margin-right: 15%;
+        font-size: 1rem;
+      }
+      .h5class{
+        font-size: 2rem;
+      }
+      .h6class{
+        font-size: 1.5rem;
+      }
+      #footerdiv{
+        font-size: 1.7rem;
+        background-color: #b392f2  !important;
+        
+      }
+      .divinfo{
+        font-size: 1.3rem !important;
+      }
+    </style>
+      
+      <!-- Custom styles for this template -->
+      <link href="../lib/Css/headers.css" rel="stylesheet">
+
+    
+    <!-- Custom styles for this template -->
+    
+    
+      
+      
+  
+  </head>
+  <body class="">
+  <div class="container">
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bd-header">
+    
+  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      
+      <span class="fs-4"> موقع أتمتة انتخابات </span>
+  </a>
+
+  <ul class="nav nav-pills">
+
+      <li class="nav-item"><a href="#" class="nav-link active "> حول الموقع </a></li>
+      <li class="nav-item"><a href="normaluser/result.php" class="nav-link">نتائج الانتخابات</a></li>
+      <li class="nav-item"><a href="sign-in.php" class="nav-link">تسجيل الدخول</a></li>
+      <li class="nav-item"><a href="#" class="nav-link ">الصفحة الرئيسية</a></li>
+      
+    </ul>
+    
+  </header>
+</div>
+<div class="information" >
+  <div  class="divinfo" >
+  <h5 class="h5class" > مقدمة  </h5>
+        هو عبارة عن موقع ويب يقوم بإدارة و تسهيل العملية الانتخابية من اجل الحصول على معلومات عن الانتخابات و النتائج كما يسمح للناخبين بالانتخاب عن طريق الموقع 
+  </div>
+        <div class="divinfo">
+        <h5 class="h5class">  اهمية الموقع  </h5>
+          يسمح للمواطنين بالانتخاب  عن طريقه  لجميع أنواع الانتخابات  <br>
+          يقوم بفرز الأصوات و تقديم الاحصائيات عن كل عملية انتخابية   <br>
+          يساعد الموظفين في العمليات الانتخابية  و الحصول على نتائج الانتخابات بدقة وسرعة <br>
+
+        </div>
+        <div class="divinfo" >
+        <h5 class="h5class" >  ميزات الموقع   </h5>
+        <h6 class="h6class" > يسمح للمواطنين بالمشاركة بالعمليات الانتخابية المتنوعة 
+            </h6 class="h6class" >
+            <h6 class="h6class" > عرض الاحصائيات  و النتيحة الخاصة بعملية انتخابية </h6>
+            <h6 class="h6class" > التحقق من هوية المستخدمين عن طريق بصمة الوجه  </h6> 
+            <br>
+          <h6 class="h6class" > ميزات مدير الموقع </h6>
+                إدارة حسابات المستخدمين
+                <br>
+                إدارة بيانات المرشحين
+                <br>
+                إدارة بيانات العمليات الانتخابية 
+                <br>
+                إدارة انوع العمليات الانتخابية 
+                <br>
+                إدارة بيانات الدوائر الانتخابية
+                <br> 
+                إدارة بيانات المناطق الانتخابية 
+                <br>
+                <h6 class="h6class" > ميزات موظف الموقع </h6>
+              إدارة بيانات المواطنين
+                <br>
+              إدارة بيانات المحافظات و المناطق السكنية
+                <br>
+              إدارة سجل القيد المدني
+                <br>
+              إدارة سجل طبيعة العمل 
+              <br>
+            </br>
+              <h6 class="h6class" >  ميزات المرشحين لعملية انتخابية </h6>
+                
+              إدارة الملف الشخصي
+                <br>
+            إدارة البرامج الانتخابية الخاصة به 
+                
+        </div>
+</div>
+
+<center>
+<div id="footerdiv"   >
+  <h4> مشروع تخرج المعهد التقاني للحاسوب جامغة البعث  </h4>
+  <p><h5 class="h6class">  إعداد الطلاب   </h5>  ميكاد الابراهيم  -  مي المحرز  - مجد الحسن </p>
+  
+</div>
+</center>
+
+    <script src="../lib/assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
